48fd987b87ab7c75d526317c4e941d3bfbbef109
mafri <47006694+mafri27@users.noreply.github.com>
2023-03-08 17:51:45 +0100

    Update snmpscan.rb

--- snmpscan.rb
+++ snmpscan.rb
@@ -6,12 +6,12 @@ require 'yaml'
 
 class Integer
     def byte_to_Mbit
-        return (self * 8 / 1024 / 1024) 
+        return (self * 8 / 1024 / 1024)
     end
 end
 
 TIOCGWINSZ = 0x5413
-page_time = 1 
+page_time = 1
 
 def get_console_cols_rows
 
@@ -67,7 +67,7 @@ m_opt = "no"  # default for line mark
 u_opt = true  # default show addition information
 
 reg_ip = Regexp.new '\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b'
-reg_ipv6 = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/ 
+reg_ipv6 = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/
 
 reg_domain = Regexp.new '^[A-Za-z0-9.-]+\.[A-Za-z]{2,5}$'
 
@@ -141,7 +141,7 @@ ARGV.each_with_index do |option , x |
         end
     end
 
-    #read inferface filter 
+    #read inferface filter
 
     if option == "-r"
         r_opt.push  ARGV[x+1]
@@ -161,7 +161,7 @@ ARGV.each_with_index do |option , x |
     end
 end
 
-# show help if host r community missing 
+# show help if host r community missing
 if h_opt == nil
     show_help()
 end
@@ -186,8 +186,8 @@ end
 puts "\e[39m\e[2J" #  set default colour and clear screen
 print "\e[1;1H\n" # jump to position 1:1
 
-devs_config = [ 
-    { 
+devs_config = [
+    {
         :name =>     '.*',
         :prio =>     0,
         :cpu_oid =>  "1.3.6.1.2.1.1.4"
@@ -207,10 +207,10 @@ devs_config = [
 end
 
 devs_config.sort! do |a , b|
-    if a[:prio] == b[:prio] 
-        a[:name] <=> b[:name] 
-    else 
-        a[:prio] <=> b[:prio] 
+    if a[:prio] == b[:prio]
+        a[:name] <=> b[:name]
+    else
+        a[:prio] <=> b[:prio]
     end
 end
 
@@ -218,8 +218,8 @@ end
 # SNMP connect
 
 begin
-    SNMP::Manager.open(:Host => h_opt , :Community => c_opt , :Timeout => 1 , :Retries => 600) do |manager| 
-        
+    SNMP::Manager.open(:Host => h_opt , :Community => c_opt , :Timeout => 1 , :Retries => 600) do |manager|
+
         #get device-name to define additional information
 
         cpu_oid = ""
@@ -237,9 +237,9 @@ begin
         devs_config.each do |dev|
             if systype =~ Regexp.new(dev[:name])
                 cpu_oid = dev[:cpu_oid] if dev[:cpu_oid]
-                filter = filter + dev[:default_filter] if dev[:default_filter] 
-                add_infos = add_infos + dev[:add_infos] if dev[:add_infos] 
-               
+                filter = filter + dev[:default_filter] if dev[:default_filter]
+                add_infos = add_infos + dev[:add_infos] if dev[:add_infos]
+
                 sec_value_factor = dev[:sec_value_factor].to_i if dev[:sec_value_factor]
                 in_sec_oct_oid = dev[:in_sec_oct_oid] if dev[:in_sec_oct_oid]
                 in_sec_pps_oid = dev[:in_sec_pps_oid] if dev[:in_sec_pps_oid]
@@ -255,7 +255,7 @@ begin
             r_opt = [ "" ]
         end
 
-        if u_opt ==  false 
+        if u_opt ==  false
             add_infos =  []
         end
 
@@ -263,8 +263,8 @@ begin
         iftable_columns << out_sec_oct_oid
         iftable_columns << in_sec_pps_oid
         iftable_columns << out_sec_pps_oid
-  
-        #search for interface with hostIP if m_opt given. write interfaceID to m_opt 
+
+        #search for interface with hostIP if m_opt given. write interfaceID to m_opt
 
         if m_opt == "yes"
             manager.walk(["1.3.6.1.2.1.4.20.1.2 ","1.3.6.1.2.1.4.20.1.1"]) do |row|
@@ -276,8 +276,8 @@ begin
 
 
         reg_interface = r_opt.map { |r| Regexp.new(r)  }
-        old = []           
-        add_oid = []      
+        old = {}
+        add_oid = []
 
         time1 = nil
 
@@ -306,7 +306,7 @@ begin
                 end
             end
 
-            # check if last lopp is less then 10 seconds in the past. 
+            # check if last lopp is less then 10 seconds in the past.
 
             if time1 != nil
                 print "       Reload:"
@@ -348,7 +348,7 @@ begin
                         add_reg = Regexp.new(relation[:test])
                         if  value.to_s =~ add_reg
 
-                            print "\e[91m" if relation[:error] == "1"
+                            print "\e[31m" if relation[:error] == "1"
                             print " #{add_info[:name]}".ljust(30)
                             print " #{relation[:output]}".ljust(30)
                             print "\e[K\n"
@@ -359,7 +359,7 @@ begin
 
                 when /max/
 
-                    print "\e[91m" if add_info[:relation].to_i < value.to_i
+                    print "\e[31m" if add_info[:relation].to_i < value.to_i
                     print " #{add_info[:name]}".ljust(30)
                     print " #{value}".ljust(30)
                     print "\e[K\n"
@@ -367,7 +367,7 @@ begin
 
                 when /min/
 
-                    print "\e[91m" if add_info[:relation].to_i > value.to_i
+                    print "\e[31m" if add_info[:relation].to_i > value.to_i
                     print " #{add_info[:name]}".ljust(30)
                     print " #{value}".ljust(30)
                     print "\e[K\n"
@@ -408,16 +408,16 @@ begin
                     old_row = old[int_id]
 
                     diffio = nil
-                    diffoo =  nil 
+                    diffoo =  nil
                     diffip = nil
                     diffop =  nil
                     diff_err_in = nil
 
-                    if old_row != nil   
+                    if old_row != nil
 
                         diffio =      (row[2].value.to_i - old_row[:ifHCInOctets].to_i  ) / interval
-                        diffoo =      (row[3].value.to_i - old_row[:ifHCOutOctets].to_i ) / interval 
-                        
+                        diffoo =      (row[3].value.to_i - old_row[:ifHCOutOctets].to_i ) / interval
+
                         diffip =      (row[4].value.to_i - old_row[:ifInUcastPkts].to_i ) / interval
                         diffip =      (row[4].value.to_i + 2**32 - old_row[:ifInUcastPkts].to_i ) / interval if diffip < 0
                         diffop =      (row[5].value.to_i - old_row[:ifOutUcastPkts].to_i) / interval
@@ -429,15 +429,15 @@ begin
 
                     diffio = row[8].value.to_i  / sec_value_factor if row[8].value.to_s  != "noSuchInstance"
                     diffoo = row[9].value.to_i  / sec_value_factor if row[9].value.to_s  != "noSuchInstance"
-                    diffip = row[10].value.to_i if row[10].value.to_s != "noSuchInstance"
-                    diffop = row[11].value.to_i if row[11].value.to_s != "noSuchInstance"
-                   
+                    diffip = row[10].value.to_i / sec_value_factor if row[10].value.to_s != "noSuchInstance"
+                    diffop = row[11].value.to_i / sec_value_factor if row[11].value.to_s != "noSuchInstance"
+
                     gray = true
                     gray = false if diffio && diffio.byte_to_Mbit > 5
                     gray = false if diffoo && diffoo.byte_to_Mbit > 5
                     gray = false if diffip && diffip > 100
                     gray = false if diffop && diffop > 100
-                    
+
                     red = false
                     red = true if diffio && diffio.byte_to_Mbit > 700
                     red = true if diffoo && diffoo.byte_to_Mbit > 700
@@ -445,12 +445,17 @@ begin
                     red = true if diffop && diffop > 100000
                     red = true if diff_err_in && diff_err_in != 0
 
-                    
-                    print "\e[1;90m"  if gray 
-                    print "\e[1;91m"  if red
-                    print "\e[1;93m"  if m_opt == row[0].value.to_i
-                    
-                    print " #{row[0].value}".ljust(11)
+
+                    print "\e[1;30m"  if gray
+                    print "\e[1;31m"  if red
+                    print "\e[33m"    if m_opt == row[0].value.to_i
+
+                    oid_id = row[0].value.to_s
+                    if oid_id.length > 7
+                        print" ..#{oid_id[-7..-1]}".ljust(11)
+                    else
+                        print" #{oid_id}".ljust(11)
+                    end
                     print "#{row[1].value[0,30]}".ljust(30)
 
                     if diffio
@@ -474,7 +479,7 @@ begin
                         print " - ".rjust(15)
                     end
                     if diff_err_in
-                        print "#{diff_err_in} ".rjust(10) 
+                        print "#{diff_err_in} ".rjust(10)
                     else
                         print " - ".rjust(10)
                     end
@@ -482,25 +487,25 @@ begin
                     print "   #{row[6].value[0,cols-115]}\e[K"
                     print "\n"
 
-                    print "\e[0;39m" 
+                    print "\e[0;39m"
 
                     #save current values to old array
-                    old[ int_id ] = { 
-                        :ifIndex => "#{row[0].value}", 
-                        :ifDescr => "#{row[1].value}", 
-                        :ifHCInOctets => "#{row[2].value}", 
-                        :ifHCOutOctets => "#{row[3].value}", 
-                        :ifInUcastPkts => "#{row[4].value}", 
-                        :ifOutUcastPkts => "#{row[5].value}", 
-                        :ifAlias => "#{row[6].value}", 
+                    old[ int_id ] = {
+                        :ifIndex => "#{row[0].value}",
+                        :ifDescr => "#{row[1].value}",
+                        :ifHCInOctets => "#{row[2].value}",
+                        :ifHCOutOctets => "#{row[3].value}",
+                        :ifInUcastPkts => "#{row[4].value}",
+                        :ifOutUcastPkts => "#{row[5].value}",
+                        :ifAlias => "#{row[6].value}",
                         :ifInErrors => "#{row[7].value}",
                     }
-                    
+
                     printed_lines += 1
-                    if printed_lines == rows - 1 
+                    if printed_lines == rows - 1
                         sleep page_time
                         paged = true
-                        printed_lines = add_infos.length + 6
+                        printed_lines = add_infos.length + 7
                         print "\e[#{printed_lines};1H\n" # jump to the start of the list
                     end
                 end
